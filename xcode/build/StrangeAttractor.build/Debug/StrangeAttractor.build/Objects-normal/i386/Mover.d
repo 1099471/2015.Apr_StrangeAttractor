03a0ba40078b951f5a9be4349ce1cc013459a11a
avseoul <av.seoul@gmail.com>
2015-05-10 17:47:00 -0400

    StrangeAttractor
    Creative Coding / Cinder / OpenGl / 2015.Apr

---
+++ xcode/build/StrangeAttractor.build/Debug/StrangeAttractor.build/Objects-normal/i386/Mover.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/avseoul/Desktop/2015_Spring_ITP/NOC/StrangeAttractor/xcode/Mover.cpp \
+  /Users/avseoul/Desktop/2015_Spring_ITP/NOC/StrangeAttractor/xcode/Mover.h \
+  ../../../../../Documents/cinder_0.8.6_mac/include/cinder/Rand.h \
+  ../../../../../Documents/cinder_0.8.6_mac/include/cinder/Perlin.h \
+  /Users/avseoul/Desktop/2015_Spring_ITP/NOC/StrangeAttractor/xcode/SpriteImage.h \
+  ../../../../../Documents/cinder_0.8.6_mac/include/cinder/gl/Texture.h \
+  /Users/avseoul/Desktop/2015_Spring_ITP/NOC/StrangeAttractor/include/Resources.h \
+  ../../../../../Documents/cinder_0.8.6_mac/include/cinder/CinderResources.h
---
+++ xcode/build/StrangeAttractor.build/Debug/StrangeAttractor.build/Objects-normal/i386/SubMover_02.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/avseoul/Desktop/2015_Spring_ITP/NOC/StrangeAttractor/xcode/SubMover_02.cpp \
+  /Users/avseoul/Desktop/2015_Spring_ITP/NOC/StrangeAttractor/xcode/SubMover_02.h \
+  ../../../../../Documents/cinder_0.8.6_mac/include/cinder/Perlin.h \
+  ../../../../../Documents/cinder_0.8.6_mac/include/cinder/Rand.h \
+  /Users/avseoul/Desktop/2015_Spring_ITP/NOC/StrangeAttractor/xcode/SpriteImage.h \
+  ../../../../../Documents/cinder_0.8.6_mac/include/cinder/gl/Texture.h \
+  /Users/avseoul/Desktop/2015_Spring_ITP/NOC/StrangeAttractor/include/Resources.h \
+  ../../../../../Documents/cinder_0.8.6_mac/include/cinder/CinderResources.h
